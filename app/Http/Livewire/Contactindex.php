9b6f717e4cfc3bbceb0e308cb01b69c88ae242d8
Aldi <20sa1132@amikompurwokert.ac.id>
2023-04-18 05:56:53 +0700

    asdasd

--- app/Http/Livewire/Contactindex.php
+++ app/Http/Livewire/Contactindex.php
@@ -1,13 +1,15 @@
 <?php
 
 namespace App\Http\Livewire;
-
+use app\Models\contact;
 use Livewire\Component;
 
 class Contactindex extends Component
 {
+    public $data;
     public function render()
     {
+        $this->data = contact::latest()->get();
         return view('livewire.contactindex');
     }
 }
--- resources/views/livewire/contactindex.blade.php
+++ resources/views/livewire/contactindex.blade.php
@@ -1,3 +1,8 @@
 <div>
     {{-- Care about people's approval and you will be their prisoner. --}}
+    <ul>
+        @foreach ($data as $item)
+            <li>{{ $item->name }}</li>
+        @endforeach
+    </ul>
 </div>
